39f854ca9b8b94c5be23d2e581b0dd6c594a4bce
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2026-02-13 16:33:56 +0545

    wip

---
+++ database/migrations/2026_02_13_104105_create_subscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('tenant_id');
+            $table->unsignedBigInteger('business_id');
+            $table->unsignedBigInteger('plan_id');
+            $table->string('billing_cycle');
+            $table->string('status');
+            $table->timestamp('renewed_at');
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
